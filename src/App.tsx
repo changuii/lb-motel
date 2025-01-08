a43cfc0a2cebf8bdaa822c44f3ce67ac4c8d8073
changuii <rhljh7410@gmail.com>
2025-01-08 14:01:18 +0900

    feat : Adding vercel anlytics tag

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import {BrowserRouter as Router} from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes.tsx";
 import Header from "./components/Header.tsx";
 import Footer from "./components/Footer.tsx";
+import {Analytics} from "@vercel/analytics/react";
 
 function App() {
 
   return (
     <>
         <Router>
+            <Analytics />
             <Header />
             <AppRoutes />
             <Footer />
